a559be977c21281b58bcf926a1530637766dc345
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-01 09:48:35 -0300

    feat: iniciando o backend do projeto

---
+++ backend/prisma/migrations/20250701124219_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "releaseDate" TIMESTAMP(3) NOT NULL,
+    "gender" TEXT NOT NULL,
+    "imageURL" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
