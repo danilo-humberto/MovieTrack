400c87a5fdfafa398a028f1320b13c6272f4f269
Danilo <danilohumberto12@hotmail.com>
2025-07-05 21:46:58 -0300

    feat: adicionando a lógica dos filtros, fazendo a chamada a api e passando para o componente de movieCard

--- frontend/src/views/home.tsx
+++ frontend/src/views/home.tsx
@@ -1,27 +1,68 @@
 import Filters from "@/components/layout/Filters";
 import MovieCard from "@/components/layout/MovieCard";
 import { Button } from "@/components/ui/button";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useMovies } from "@/hooks/useMovies";
+import type { Movie } from "@/types/Movie";
+import { ArrowLeft, ArrowRight, LoaderCircle } from "lucide-react";
 import { useState } from "react";
 
 const Home = () => {
-  const moviesMock = new Array(8).fill(null).map((_, i) => ({ id: i + 1 }));
+  const { data, isLoading, isError } = useMovies();
+  const [selectedGender, setSelectedGender] = useState("");
+  const [searchText, setSearchText] = useState("");
+
+  const movies = data ?? [];
+
+  const filteredMovies = movies.filter((movie: Movie) => {
+    const matchsGender = selectedGender
+      ? movie.gender === selectedGender
+      : true;
+    const matchesSearch = movie.title
+      .toLowerCase()
+      .includes(searchText.toLowerCase());
+
+    return matchsGender && matchesSearch;
+  });
+
   const [currentPage, setCurrentPage] = useState(1);
   const moviesPerPage = 8;
-  const totalPages = Math.ceil(moviesMock.length / moviesPerPage);
-  const paginatedMovies = moviesMock.slice(
-    (currentPage - 1) * moviesPerPage,
-    currentPage * moviesPerPage
-  );
+  const totalPages = data
+    ? Math.ceil(filteredMovies.length / moviesPerPage)
+    : 0;
+  const paginatedMovies = filteredMovies
+    ? filteredMovies.slice(
+        (currentPage - 1) * moviesPerPage,
+        currentPage * moviesPerPage
+      )
+    : [];
 
   return (
     <>
-      <Filters />
+      <Filters
+        movies={movies}
+        onGenderChange={setSelectedGender}
+        onSearchChange={setSearchText}
+      />
       <div>
-        {moviesMock.length > 0 ? (
+        {isLoading ? (
+          <div className="w-full flex items-center justify-center mt-20">
+            <LoaderCircle size={30} className="animate-spin" />
+          </div>
+        ) : isError ? (
+          <div className="w-full flex items-center justify-center mt-20">
+            <h2 className="text-muted-foreground">Erro ao carregar filmes.</h2>
+          </div>
+        ) : paginatedMovies.length > 0 ? (
           <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4 md:max-w-[70%] m-auto">
-            {paginatedMovies.map((movie) => (
-              <MovieCard key={movie.id} />
+            {paginatedMovies.map((movie: Movie) => (
+              <MovieCard
+                key={movie.id}
+                title={movie.title}
+                releaseYear={movie.releaseYear}
+                gender={movie.gender}
+                duration={movie.duration}
+                imageUrl={movie.imageUrl}
+              />
             ))}
           </div>
         ) : (
