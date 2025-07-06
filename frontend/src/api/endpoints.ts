9aebc2077d557ef62ab8e5405ed1e2d10c06a657
Danilo <danilohumberto12@hotmail.com>
2025-07-05 21:44:44 -0300

    feat: criando as conexõs com os endpoints

---
+++ frontend/src/api/endpoints.ts
@@ -0,0 +1,26 @@
+import api from "./axios";
+
+export const getMovies = async () => {
+  const { data } = await api.get("/movies");
+  return data;
+};
+
+export const getMovieById = async (id: string) => {
+  const { data } = await api.get(`/movies/${id}`);
+  return data;
+};
+
+export const createMovie = async (movie: any) => {
+  const { data } = await api.post("/movies", movie);
+  return data;
+};
+
+export const updateMovie = async (id: string, movie: any) => {
+  const { data } = await api.patch(`/movies/${id}`, movie);
+  return data;
+};
+
+export const deleteMovie = async (id: string) => {
+  const { data } = await api.delete(`/movies/${id}`);
+  return data;
+};
