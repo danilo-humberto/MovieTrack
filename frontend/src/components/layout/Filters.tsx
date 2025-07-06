de9ff411d3515a16c309be9b6d09c4a47451daee
Danilo <danilohumberto12@hotmail.com>
2025-07-05 21:46:13 -0300

    fix: alterando a lógica para fazer os filtros funcionarem

--- frontend/src/components/layout/Filters.tsx
+++ frontend/src/components/layout/Filters.tsx
@@ -9,8 +9,31 @@ import {
 } from "../ui/select";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
+import type { Movie } from "@/types/Movie";
+import { useState } from "react";
+import { Separator } from "../ui/separator";
 
-const Filters = () => {
+interface FiltersProps {
+  movies: Movie[];
+  onGenderChange: (gender: string) => void;
+  onSearchChange: (search: string) => void;
+}
+
+const Filters = ({ movies, onGenderChange, onSearchChange }: FiltersProps) => {
+  const [selectedGender, setSelectedGender] = useState<string | undefined>("");
+  const uniqueGenres = Array.from(
+    new Set(movies.map((movie: Movie) => movie.gender))
+  );
+
+  const handleGenderChange = (gender: string) => {
+    if (gender === "clear") {
+      setSelectedGender("");
+      onGenderChange("");
+    } else {
+      setSelectedGender(gender);
+      onGenderChange(gender);
+    }
+  };
   const navigate = useNavigate();
   return (
     <div className="p-4 flex flex-col gap-3 md:flex-row md:max-w-[70%] m-auto">
@@ -20,17 +43,25 @@ const Filters = () => {
           type="text"
           placeholder="Pesquisar filmes..."
           className="text-sm outline-none w-full border-none pr-2 placeholder:text-muted-foreground"
+          onChange={(e) => onSearchChange(e.target.value)}
         />
       </div>
       <div className="flex items-center gap-4 flex-1">
-        <Select>
+        <Select value={selectedGender} onValueChange={handleGenderChange}>
           <SelectTrigger className="flex-1 bg-transparent">
             <SelectValue placeholder="Gênero" />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
-              <SelectItem value="action">Ação</SelectItem>
-              <SelectItem value="comedy">Comédia</SelectItem>
+              {uniqueGenres.map((gender: string) => (
+                <SelectItem key={gender} value={gender}>
+                  {gender}
+                </SelectItem>
+              ))}
+              <Separator />
+              <SelectItem value="clear" disabled={selectedGender === ""}>
+                Limpar
+              </SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
