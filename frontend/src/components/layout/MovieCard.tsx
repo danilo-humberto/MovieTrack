6b71fd6375cda32fc928d3b4e2577151ef50c1d9
Danilo <danilohumberto12@hotmail.com>
2025-07-05 21:47:17 -0300

    fix: alterando o código para apresentar os dados vindo da api

--- frontend/src/components/layout/MovieCard.tsx
+++ frontend/src/components/layout/MovieCard.tsx
@@ -1,28 +1,43 @@
+import { formatDuration } from "@/utils/formatDuration";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
 import { Card, CardContent } from "../ui/card";
 import { Separator } from "../ui/separator";
 
 // LEMBRAR DE VERIFICAR SE TEM IMAGEM, SE NÃO TIVER, USAR UMA IMAGEM PADRÃO
+interface MovieCardProps {
+  title: string;
+  releaseYear: number;
+  gender: string;
+  duration: number;
+  imageUrl: string;
+}
 
-const MovieCard = () => {
+const MovieCard = ({
+  title,
+  releaseYear,
+  gender,
+  duration,
+  imageUrl,
+}: MovieCardProps) => {
   return (
     <Card className="relative">
       <CardContent className="w-full h-full">
         <img
-          src="https://play-lh.googleusercontent.com/SJ7njnkJjxcJwQek6J0mAF7UzvYhJ-RXYOMLY9E-g1wwWHgwMrqMWwImf-9271CMiHEVS7r2MWbPmadxErRY"
-          alt=""
+          src={imageUrl}
+          alt={title}
+          onError={(e) => (e.currentTarget.src = "/Image-not-found.png")}
           className="w-full h-full object-fit brightness-[65%] hover:scale-105 transition-all duration-300 ease-in-out"
         />
         <div className="absolute bottom-4 flex flex-col gap-4 text-background px-4 w-full">
-          <h2 className="font-semibold text-xl truncate">Matrix</h2>
+          <h2 className="font-semibold text-xl truncate">{title}</h2>
           <div className="flex justify-between items-center flex-wrap gap-2">
             <div className="flex gap-2 ">
-              <Badge variant="default">Ação</Badge>
+              <Badge variant="default">{gender}</Badge>
               <div className="flex gap-2 font-semibold">
-                <span className="text-sm">1999</span>
+                <span className="text-sm">{releaseYear}</span>
                 <Separator orientation="vertical" />
-                <span className="text-sm">2h 16m</span>
+                <span className="text-sm">{formatDuration(duration)}</span>
               </div>
             </div>
             <a href="/details" className="w-full">
