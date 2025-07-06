7a36d63e0eceb867af27a67a8a22e763d06a5961
Danilo <danilohumberto12@hotmail.com>
2025-07-05 21:46:29 -0300

    feat: adicionado o type do Movie que vai vir do back

---
+++ frontend/src/types/Movie.ts
@@ -0,0 +1,11 @@
+export type Movie = {
+  id: string;
+  title: string;
+  description: string;
+  releaseYear: number;
+  gender: string;
+  director: string;
+  duration: number;
+  imageUrl: string;
+  trailerUrl: string;
+};
