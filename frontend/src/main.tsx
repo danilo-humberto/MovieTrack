56c9a4d27a372b135fc220d16543123c844bd8c1
Danilo <danilohumberto12@hotmail.com>
2025-07-05 21:45:31 -0300

    feat: adicionando o react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryCliente = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Toaster position="top-right" />
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryCliente}>
+      <BrowserRouter>
+        <Toaster position="top-right" />
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
