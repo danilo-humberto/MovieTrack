56b4712c5fea4f1b2381100de70f962a4c2bf828
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-07 14:23:50 -0300

    fix: adicionando um novo estilo

--- frontend/src/components/layout/MovieCard.tsx
+++ frontend/src/components/layout/MovieCard.tsx
@@ -24,15 +24,19 @@ const MovieCard = ({
   imageUrl,
 }: MovieCardProps) => {
   return (
-    <Card className="relative">
-      <CardContent className="w-full h-[400px]">
+    <Card className="relative group overflow-hidden">
+      <CardContent className="w-full h-full">
         <img
           src={imageUrl}
           alt={title}
           onError={(e) => (e.currentTarget.src = "/Image-not-found.png")}
-          className="w-full h-full object-fit object-center brightness-[65%] hover:scale-105 transition-all duration-300 ease-in-out"
+          className="w-full h-full object-fit object-center brightness-[65%]"
         />
-        <div className="absolute bottom-4 flex flex-col gap-4 text-background px-4 w-full">
+        <div
+          className="absolute bottom-4 left-0 px-3 w-full flex flex-col gap-4 text-background 
+                 opacity-100 group-hover:opacity-100 transition-opacity duration-300 
+                 md:opacity-0"
+        >
           <h2 className="font-semibold text-xl truncate">{title}</h2>
           <div className="flex justify-between items-center flex-wrap gap-2">
             <div className="flex gap-2 ">
