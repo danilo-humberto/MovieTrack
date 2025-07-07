6a3656e8e6796f518791bace4213a139303cc69d
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-07 14:23:19 -0300

    fix: alterando a chamada da imagem

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="sticky top-0 flex items-center justify-center bg-[#252A2F] border-b border-muted-foreground z-50">
       <img
-        src="./logo.png"
+        src="/logo.png"
         alt="logo do site movie track"
         className="w-40 h-20 object-contain"
       />
