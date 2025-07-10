ca181ba21bcbe7588bad8d05f3539cdd8d269fde
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-10 08:51:09 -0300

    fix: ajustando para quando clicar na logo, redirecionar para a home

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <header className="sticky top-0 flex items-center justify-center bg-[#252A2F] border-b border-muted-foreground z-50">
       <img
         src="/logo.png"
         alt="logo do site movie track"
-        className="w-40 h-20 object-contain"
+        className="w-40 h-20 object-contain cursor-pointer"
+        onClick={() => {
+          navigate("/");
+        }}
       />
     </header>
   );
