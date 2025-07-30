090beba65397e8430e82982a5cb14f708f431a4b
Danilo <danilohumberto12@hotmail.com>
2025-07-30 19:52:10 -0300

    fix: ajustando o fallback de erro quando insere uma url de imagem inválida

--- frontend/src/components/layout/MovieCard.tsx
+++ frontend/src/components/layout/MovieCard.tsx
@@ -4,6 +4,8 @@ import { Button } from "../ui/button";
 import { Card, CardContent } from "../ui/card";
 import { Separator } from "../ui/separator";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Loader } from "lucide-react";
 
 // LEMBRAR DE VERIFICAR SE TEM IMAGEM, SE NÃO TIVER, USAR UMA IMAGEM PADRÃO
 interface MovieCardProps {
@@ -23,15 +25,33 @@ const MovieCard = ({
   duration,
   imageUrl,
 }: MovieCardProps) => {
+  const [imgError, setImgError] = useState(false);
+  const [imgLoaded, setImgLoaded] = useState(false);
   return (
     <Card className="relative group overflow-hidden">
       <CardContent className="w-full h-full">
-        <img
-          src={imageUrl}
-          alt={title}
-          onError={(e) => (e.currentTarget.src = "/Image-not-found.png")}
-          className="w-full h-full object-fit object-center brightness-[65%]"
-        />
+        {!imgLoaded && !imgError && (
+          <div className="w-full h-full bg-muted-foreground flex items-center justify-center flex-col">
+            <Loader className="animate-spin" />
+            carregando imagem...
+          </div>
+        )}
+        {imgError && (
+          <div className="w-full h-full bg-muted-foreground flex items-center justify-center flex-col">
+            <p>Erro ao carregar a imagem!</p>
+            <span className="text-xs">Edite a URL e tente novamente!</span>
+          </div>
+        )}
+
+        {!imgError && (
+          <img
+            src={imageUrl}
+            alt={title}
+            onError={() => setImgError(true)}
+            onLoad={() => setImgLoaded(true)}
+            className="w-full h-full object-fit object-center brightness-[65%]"
+          />
+        )}
         <div
           className="absolute bottom-4 left-0 px-3 w-full flex flex-col gap-4 text-background 
                     transition-all duration-500 ease-in-out translate-y-0 opacity-100
